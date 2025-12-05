981b01808d6ccf2a9024fe517132625ec33e433a
Lucas Faria Mendes <lucas.oliveira1676@etec.sp.gov.br>
2025-12-05 01:40:23 -0300

    print a prompt

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,5 @@ var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
-    // try stdout.print("$ ", .{});
+    try stdout.print("$ ", .{});
 }
