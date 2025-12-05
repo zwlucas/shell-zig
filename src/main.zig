62ad0883af03ca81bbef295468d523d0995aa1b1
Lucas Faria Mendes <lucas.oliveira1676@etec.sp.gov.br>
2025-12-05 02:01:52 -0300

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,9 @@ pub fn main() !void {
 
         const command = try stdin.takeDelimiter('\n');
         if (command) |cmd| {
+            if (std.mem.eql(u8, cmd, "exit")) {
+                break;
+            }
             try stdout.print("{s}: command not found\n", .{cmd});
         } else {
             break;
